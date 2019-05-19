76c329db2692dbdb8283db48dd8710f75af20612
Shubham Yadav <shubhamyadav2002@gmail.com>
2019-05-20 01:23:04 +0530

    Commit no. 4

--- git_commit.py
+++ git_commit.py
@@ -1,5 +1,7 @@
+from pynput.keyboard import Key, Controller
 import os
 
+keyboard = Controller()
 f = open("commit_no.txt", "r")
 x = f.read()
 f.close()
@@ -7,6 +9,9 @@ x = int(x)+1
 os.system("git add .")
 os.system("git commit -m \"Commit no. "+str(x)+"\"")
 os.system("git push origin master")
+keyboard.type("shubhamamsa")
+keyboard.press("Key.enter")
+keyboard.release("Key.enter")
 f = open("commit_no.txt", "w")
 f.write(str(x))
 f.close()
